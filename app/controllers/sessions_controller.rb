88818f9f66662ea7bca8da1a588596620665adf9
Bogdasya <bogdasya93@gmail.com>
2015-12-02 20:09:50 +0200

    Adding login/sign_up

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < Devise::SessionsController
+  skip_before_filter :verify_authenticity_token, :only => :destroy
+
+  def create
+    if User.find_by(email: params[:user][:email])
+      self.resource = warden.authenticate!(auth_options)
+      sign_in(resource_name, resource)
+      respond_to do |format|
+        format.js { render action: 'login' }
+      end
+    else
+      build_resource(sign_up_params)
+
+      resource.save
+      if resource.persisted?
+        if resource.active_for_authentication?
+          sign_in(resource_name, resource)
+          redirect_to root_path
+        else
+          expire_data_after_sign_in!
+          redirect_to root_path
+        end
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        end
+      end
+    end
+
+  private
+  def build_resource(hash=nil)
+    self.resource = resource_class.new_with_session(hash || {}, session)
+  end
+
+  def sign_up_params
+    devise_parameter_sanitizer.sanitize(:sign_up)
+  end
+end
