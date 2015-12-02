f7d3268bbca8d2e91cd573b1bd22856a8903e85f
Bogdasya <bogdasya93@gmail.com>
2015-12-02 20:25:32 +0200

    Update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
   end
 
+  def update
+    binding.pry
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to :back
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :email, :last_name, :mobile_phone)
+  end
 end
